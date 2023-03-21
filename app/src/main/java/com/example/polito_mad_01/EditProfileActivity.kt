20ae9f300fcd1f5ad0aff2f499a3f10ad9be8d2b
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-03-21 11:17:50 +0100

    menu update

--- app/src/main/java/com/example/polito_mad_01/EditProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/EditProfileActivity.kt
@@ -1,11 +1,32 @@
 package com.example.polito_mad_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_edit_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        saveData()
+        val i = Intent(this, ShowProfileActivity::class.java)
+        startActivity(i)
+        return true
+    }
+
+    private fun saveData(){
+
     }
 }
--- app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
@@ -1,7 +1,11 @@
 package com.example.polito_mad_01
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 
 class ShowProfileActivity : AppCompatActivity() {
@@ -9,4 +13,18 @@ class ShowProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_show_profile, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val i = Intent(this, EditProfileActivity::class.java)
+        startActivity(i)
+        return true
+    }
+
+
 }
