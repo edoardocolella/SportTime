8dab9af7529616494da4a8c493e5f881af16af2d
antonio-campobasso <Runes_Ech0>
2023-03-21 10:17:55 +0100

    no bar bye bye :)

--- app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
@@ -2,6 +2,7 @@ package com.example.polito_mad_01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
