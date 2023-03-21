84b00bd99ded83150cb76cf77cbd6ea986888e0f
antonio-campobasso <Runes_Ech0>
2023-03-21 11:19:24 +0100

    test for layout

--- app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
@@ -1,12 +1,21 @@
 package com.example.polito_mad_01
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.LinearLayout
 
 class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+//
+//        val layout: LinearLayout = findViewById(R.id.infoLayout)
+//        if(resources.configuration.orientation == Configuration.ORIENTATION_LANDSCAPE){
+//            layout.orientation =
+//        } else {
+//
+//        }
     }
 }
