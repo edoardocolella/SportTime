824110f6a01682d424e9a5d839be81d299895d73
antonio <s294714@studenti.polito.it>
2023-04-03 12:50:49 +0200

    small improved save/get function

--- app/src/main/java/com/example/polito_mad_01/EditProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/EditProfileActivity.kt
@@ -67,7 +67,6 @@ class EditProfileActivity : AppCompatActivity() {
 
     }
 
-
     override fun onCreateContextMenu(
         menu: ContextMenu?,
         v: View?,
@@ -77,7 +76,6 @@ class EditProfileActivity : AppCompatActivity() {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_picture, menu)
     }
-
 
     private fun openCamera() {
         val values = ContentValues()
@@ -214,21 +212,21 @@ class EditProfileActivity : AppCompatActivity() {
         val sp = getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE).edit()
         val user = JSONObject()
         user.put("fullName", findViewById<EditText>(R.id.fullName_value).text)
-        .put("nickname", findViewById<EditText>(R.id.nickName_value).text)
+            .put("nickname", findViewById<EditText>(R.id.nickName_value).text)
             .put("description", findViewById<EditText>(R.id.description_value).text)
             .put("age", findViewById<EditText>(R.id.age_value).text)
-        .put("gender", findViewById<EditText>(R.id.gender_value).text)
-        .put("location", findViewById<EditText>(R.id.location_value).text)
-        .put("monday", findViewById<EditText>(R.id.monHours_value).text)
-        .put("tuesday", findViewById<EditText>(R.id.tueHours_value).text)
-        .put("wednesday", findViewById<EditText>(R.id.wedHours_value).text)
-        .put("thursday", findViewById<EditText>(R.id.thuHours_value).text)
-        .put("friday", findViewById<EditText>(R.id.friHours_value).text)
-        .put("saturday", findViewById<EditText>(R.id.satHours_value).text)
-        .put("sunday", findViewById<EditText>(R.id.sunHours_value).text)
-        .put("phoneNumber", findViewById<EditText>(R.id.phoneNumber_value).text)
-        .put("email", findViewById<EditText>(R.id.mail_value).text)
-        //.put("image_data", encodedImage )
+            .put("gender", findViewById<EditText>(R.id.gender_value).text)
+            .put("location", findViewById<EditText>(R.id.location_value).text)
+            .put("monday", findViewById<EditText>(R.id.monHours_value).text)
+            .put("tuesday", findViewById<EditText>(R.id.tueHours_value).text)
+            .put("wednesday", findViewById<EditText>(R.id.wedHours_value).text)
+            .put("thursday", findViewById<EditText>(R.id.thuHours_value).text)
+            .put("friday", findViewById<EditText>(R.id.friHours_value).text)
+            .put("saturday", findViewById<EditText>(R.id.satHours_value).text)
+            .put("sunday", findViewById<EditText>(R.id.sunHours_value).text)
+            .put("phoneNumber", findViewById<EditText>(R.id.phoneNumber_value).text)
+            .put("email", findViewById<EditText>(R.id.mail_value).text)
+            //.put("image_data", encodedImage )
 
 
         sp.putString("user", user.toString())
@@ -239,24 +237,31 @@ class EditProfileActivity : AppCompatActivity() {
         val sp = getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)
 
         //extract a json object from a string
-        val userString = sp.getString("user", "{\"fullName\":\"Mario Rossi\",\"description\":\"I'm a student\",\"age\":20,\"nickname\":\"m@r1o_ross1\",\"age\":20,\"gender\":\"M\",\"location\":\"Torino\",\"monday\":\"8:00-12:00\",\"tuesday\":\"8:00-12:00\",\"wednesday\":\"8:00-12:00\",\"thursday\":\"8:00-12:00\",\"friday\":\"8:00-12:00\",\"saturday\":\"8:00-12:00\",\"sunday\":\"8:00-12:00\",\"phoneNumber\": \"1234567890\",\"email\": \"mario.rossi@email.it\"}")
-        val userObject = JSONObject(userString!!)
+        val userString = sp.getString("user", null)
+        userString?.let {
+            val userObject = JSONObject(userString)
 
-        findViewById<TextView>(R.id.fullName_value).text = userObject.getString("fullName")
-        findViewById<TextView>(R.id.nickName_value).text = userObject.getString("nickname")
-        findViewById<TextView>(R.id.age_value).text = userObject.getInt("age").toString()
-        findViewById<TextView>(R.id.description_value).text = userObject.getString("description")
-        findViewById<TextView>(R.id.gender_value).text = userObject.getString("gender")
-        findViewById<TextView>(R.id.location_value).text =userObject.getString("location")
-        findViewById<TextView>(R.id.monHours_value).text =userObject.getString("monday")
-        findViewById<TextView>(R.id.tueHours_value).text =userObject.getString("tuesday")
-        findViewById<TextView>(R.id.wedHours_value).text =userObject.getString("wednesday")
-        findViewById<TextView>(R.id.thuHours_value).text =userObject.getString("thursday")
-        findViewById<TextView>(R.id.friHours_value).text =userObject.getString("friday")
-        findViewById<TextView>(R.id.satHours_value).text =userObject.getString("saturday")
-        findViewById<TextView>(R.id.sunHours_value).text =userObject.getString("sunday")
-        findViewById<TextView>(R.id.mail_value).text =userObject.getString("email")
-        findViewById<TextView>(R.id.phoneNumber_value).text =userObject.getString("phoneNumber")
+            findViewById<TextView>(R.id.fullName_value).text = userObject.getString("fullName")
+            findViewById<TextView>(R.id.nickName_value).text = userObject.getString("nickname")
+            findViewById<TextView>(R.id.age_value).text = userObject.getInt("age").toString()
+            findViewById<TextView>(R.id.description_value).text = userObject.getString("description")
+            findViewById<TextView>(R.id.gender_value).text = userObject.getString("gender")
+            findViewById<TextView>(R.id.location_value).text =userObject.getString("location")
+            findViewById<TextView>(R.id.monHours_value).text =userObject.getString("monday")
+            findViewById<TextView>(R.id.tueHours_value).text =userObject.getString("tuesday")
+            findViewById<TextView>(R.id.wedHours_value).text =userObject.getString("wednesday")
+            findViewById<TextView>(R.id.thuHours_value).text =userObject.getString("thursday")
+            findViewById<TextView>(R.id.friHours_value).text =userObject.getString("friday")
+            findViewById<TextView>(R.id.satHours_value).text =userObject.getString("saturday")
+            findViewById<TextView>(R.id.sunHours_value).text =userObject.getString("sunday")
+            findViewById<TextView>(R.id.mail_value).text =userObject.getString("email")
+            findViewById<TextView>(R.id.phoneNumber_value).text =userObject.getString("phoneNumber")
+        }
+
+        /*var test :String? = userObject.getString("image_data")
+            val b: ByteArray = Base64.decode(test, Base64.DEFAULT)
+            val bitmap = BitmapFactory.decodeByteArray(b, 0, b.size)
+            findViewById<ImageView>(R.id.profileImage_imageView).setImageBitmap(bitmap)*/
     }
 }
 
--- app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/ShowProfileActivity.kt
@@ -64,28 +64,30 @@ class ShowProfileActivity : AppCompatActivity() {
         val sp = getSharedPreferences("mySharedPreferences", Context.MODE_PRIVATE)
 
         //extract a json object from a string
-        val userString = sp.getString("user", "{\"fullName\":\"Mario Rossi\",\"nickname\":\"C1cal0ne\",\"description\":\"Best striker Cenisia\",\"age\":34,\"gender\":\"M\",\"location\":\"Torino\",\"monday\":\"8:00-12:00\",\"tuesday\":\"8:00-12:00\",\"wednesday\":\"8:00-12:00\",\"thursday\":\"8:00-12:00\",\"friday\":\"8:00-12:00\",\"saturday\":\"8:00-12:00\",\"sunday\":\"8:00-12:00, 16:00-20:00\",\"phoneNumber\": \"1234567890\",\"email\": \"mario.rossi@email.it\"}")
-        val userObject = JSONObject(userString!!)
+        val userString = sp.getString("user", null)
+        userString?.let {
+            val userObject = JSONObject(userString)
 
-        findViewById<TextView>(R.id.fullName_textView2).text = userObject.getString("fullName")
-        findViewById<TextView>(R.id.nickName_textView).text = userObject.getString("nickname")
-        findViewById<TextView>(R.id.age_textView).text = userObject.getString("age")
-        findViewById<TextView>(R.id.description_textView).text = userObject.getString("description")
-        findViewById<TextView>(R.id.gender_textView).text = userObject.getString("gender")
-        findViewById<TextView>(R.id.location_textView).text =userObject.getString("location")
-        findViewById<TextView>(R.id.monHours_textView).text =userObject.getString("monday")
-        findViewById<TextView>(R.id.tueHours_textView).text =userObject.getString("tuesday")
-        findViewById<TextView>(R.id.wedHours_textView).text =userObject.getString("wednesday")
-        findViewById<TextView>(R.id.thuHours_textView).text =userObject.getString("thursday")
-        findViewById<TextView>(R.id.friHours_textView).text =userObject.getString("friday")
-        findViewById<TextView>(R.id.satHours_textView).text =userObject.getString("saturday")
-        findViewById<TextView>(R.id.sunHours_textView).text =userObject.getString("sunday")
-        findViewById<TextView>(R.id.mail_textView).text =userObject.getString("email")
-        findViewById<TextView>(R.id.phoneNumber_textView).text =userObject.getString("phoneNumber")
-        /*var test :String? = userObject.getString("image_data")
-        val b: ByteArray = Base64.decode(test, Base64.DEFAULT)
-        val bitmap = BitmapFactory.decodeByteArray(b, 0, b.size)
-        findViewById<ImageView>(R.id.profileImage_imageView).setImageBitmap(bitmap)*/
+            findViewById<TextView>(R.id.fullName_value).text = userObject.getString("fullName")
+            findViewById<TextView>(R.id.nickName_value).text = userObject.getString("nickname")
+            findViewById<TextView>(R.id.age_value).text = userObject.getInt("age").toString()
+            findViewById<TextView>(R.id.description_value).text = userObject.getString("description")
+            findViewById<TextView>(R.id.gender_value).text = userObject.getString("gender")
+            findViewById<TextView>(R.id.location_value).text =userObject.getString("location")
+            findViewById<TextView>(R.id.monHours_value).text =userObject.getString("monday")
+            findViewById<TextView>(R.id.tueHours_value).text =userObject.getString("tuesday")
+            findViewById<TextView>(R.id.wedHours_value).text =userObject.getString("wednesday")
+            findViewById<TextView>(R.id.thuHours_value).text =userObject.getString("thursday")
+            findViewById<TextView>(R.id.friHours_value).text =userObject.getString("friday")
+            findViewById<TextView>(R.id.satHours_value).text =userObject.getString("saturday")
+            findViewById<TextView>(R.id.sunHours_value).text =userObject.getString("sunday")
+            findViewById<TextView>(R.id.mail_value).text =userObject.getString("email")
+            findViewById<TextView>(R.id.phoneNumber_value).text =userObject.getString("phoneNumber")
+            /*var test :String? = userObject.getString("image_data")
+            val b: ByteArray = Base64.decode(test, Base64.DEFAULT)
+            val bitmap = BitmapFactory.decodeByteArray(b, 0, b.size)
+            findViewById<ImageView>(R.id.profileImage_imageView).setImageBitmap(bitmap)*/
+        }
     }
 
 
