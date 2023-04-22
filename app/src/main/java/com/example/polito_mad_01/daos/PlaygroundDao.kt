5f4209a42e7f5cae3b0c15f056c138d469fb0b02
Lorenzo <lory2098@libero.it>
2023-04-22 10:52:39 +0200

    Added some queries in DAOs

--- app/src/main/java/com/example/polito_mad_01/daos/PlaygroundDao.kt
+++ app/src/main/java/com/example/polito_mad_01/daos/PlaygroundDao.kt
@@ -1,13 +1,18 @@
 package com.example.polito_mad_01.daos
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.example.polito_mad_01.model.Playground
+import com.example.polito_mad_01.model.User
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface PlaygroundDao {
     @Query("SELECT * from playground")
     fun getAllPlaygrounds(): Flow<List<Playground>>
+
+    @Query("SELECT * from user where playground_id = :playgroundId")
+    fun getPlaygroundById(playgroundId: Int): LiveData<Playground>
 }
 
--- app/src/main/java/com/example/polito_mad_01/daos/SportDao.kt
+++ app/src/main/java/com/example/polito_mad_01/daos/SportDao.kt
@@ -1,7 +1,9 @@
 package com.example.polito_mad_01.daos
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
+import com.example.polito_mad_01.model.Playground
 import com.example.polito_mad_01.model.Sport
 import kotlinx.coroutines.flow.Flow
 
@@ -9,5 +11,8 @@ import kotlinx.coroutines.flow.Flow
 interface SportDao {
     @Query("SELECT * from sport")
     fun getAllSports(): Flow<List<Sport>>
+
+    @Query("SELECT * from sport where sport_id = :sportId")
+    fun getSportById(sportId: Int): LiveData<Sport>
 }
 
