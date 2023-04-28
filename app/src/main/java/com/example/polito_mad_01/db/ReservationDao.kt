9189981332579c3f2fee201b5470a27513056f64
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-04-28 10:58:31 +0200

    slot with reservation

--- app/src/main/java/com/example/polito_mad_01/db/ReservationDao.kt
+++ app/src/main/java/com/example/polito_mad_01/db/ReservationDao.kt
@@ -10,7 +10,15 @@ interface ReservationDao {
 
     @Query("SELECT * from slot where user_id = :user_id and is_reserved = true")
     fun getReservationByUserId(user_id: Int): Flow<List<Slot>>
-    
+
+    /** @param date format: yyyy-MM-dd*/
+    @Transaction
+    @Query("SELECT * from slot " +
+            "INNER JOIN playground ON slot.playground_id = playground.playground_id " +
+            "where is_reserved = false " +
+            "and sport_name = :sport_name " +
+            "and date > :date")
+    fun getFreeSlots(sport_name:String, date:String): Flow<List<SlotWithPlayground>>
 
 }
 
