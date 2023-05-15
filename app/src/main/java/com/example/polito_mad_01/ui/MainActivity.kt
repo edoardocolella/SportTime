8c72c2ac65091cadd3e83a15768dfcc108cb818a
Edoardo Colella <edoardocolella.it@gmail.com>
2023-05-15 23:57:32 +0200

    feat: nav header working

--- app/src/main/java/com/example/polito_mad_01/ui/MainActivity.kt
+++ app/src/main/java/com/example/polito_mad_01/ui/MainActivity.kt
@@ -4,11 +4,13 @@ import android.Manifest
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.MenuItem
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.appcompat.app.*
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
-import com.example.polito_mad_01.R
+import com.example.polito_mad_01.*
+import com.example.polito_mad_01.viewmodel.*
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var toggle: ActionBarDrawerToggle
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navView: NavigationView
+
+    private val vm: MainActivityViewModel by viewModels {
+        MainActivityViewModelFactory((application as SportTimeApplication).userRepository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +41,14 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawerLayout)
         navView = findViewById(R.id.navView)
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+
+
+        vm.getUser(1).observe(this) {
+            val view = navView.getHeaderView(0)
+            val nameSurname = "${it.name} ${it.surname}"
+            view.findViewById<TextView>(R.id.nameNav).text = nameSurname
+            view.findViewById<TextView>(R.id.usernameNav).text = it.nickname
+        }
 
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
