771be56f5fa8b7085e799e86a56d893f40ef9030
Edoardo Colella s257113 <s257113@studenti.polito.it>
2023-05-30 17:13:08 +0200

    update

--- app/src/main/java/com/example/polito_mad_01/ui/Step2Fragment.kt
+++ app/src/main/java/com/example/polito_mad_01/ui/Step2Fragment.kt
@@ -1,16 +1,21 @@
 package com.example.polito_mad_01.ui
 
 import android.annotation.SuppressLint
+import android.icu.text.SimpleDateFormat
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.polito_mad_01.R
+import com.example.polito_mad_01.util.UIUtils
 import com.example.polito_mad_01.viewmodel.RegistrationViewModel
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.util.*
