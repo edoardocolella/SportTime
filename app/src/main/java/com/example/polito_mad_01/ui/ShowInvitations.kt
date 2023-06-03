d7b8a1ac35a9ba13d9cae00cd0cd58dd5d017484
antonio <s294714@studenti.polito.it>
2023-06-03 17:12:00 +0200

    game invitation data passed

--- app/src/main/java/com/example/polito_mad_01/ui/ShowInvitations.kt
+++ app/src/main/java/com/example/polito_mad_01/ui/ShowInvitations.kt
@@ -1,13 +1,44 @@
 package com.example.polito_mad_01.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.polito_mad_01.R
+import com.example.polito_mad_01.adapters.FriendRequestAdapter
+import com.example.polito_mad_01.util.UIUtils
 import com.example.polito_mad_01.viewmodel.InvitationsViewModel
 
 class ShowInvitations(val vm: InvitationsViewModel) : Fragment(R.layout.fragment_show_invitations) {
+    private lateinit var recyclerViewGameInvitations: RecyclerView
+    private lateinit var noInvitations: TextView
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //noInvitations = UIUtils.findTextViewById(view, R.id.noRequestsTextView)!!
+        //noInvitations.visibility= View.GONE
+        //recyclerViewGameInvitations =view.findViewById(R.id.friendRequestsRecyclerView)
+        //recyclerViewGameInvitations.layoutManager = LinearLayoutManager(view.context)
+
+
+        setAllView()
+    }
+
+    private fun setAllView() {
+        vm.getUserInvitations().observe(viewLifecycleOwner) { invitations ->
+            println("INVITATIONS ${invitations.map { it.sender.name }}")
+/*            recyclerViewGameInvitations.adapter= FriendRequestAdapter(friendRequestsNickname, vm)
+
+            if (friendRequestsNickname.isEmpty()) {
+                recyclerViewGameInvitations.visibility = View.GONE
+                noInvitations.visibility = View.VISIBLE
+            } else {
+                recyclerViewGameInvitations.visibility = View.VISIBLE
+                noInvitations.visibility = View.GONE
+            }*/
+        }
+    }
 }
--- app/src/main/java/com/example/polito_mad_01/viewmodel/InvitationsViewModel.kt
+++ app/src/main/java/com/example/polito_mad_01/viewmodel/InvitationsViewModel.kt
@@ -5,17 +5,23 @@ import androidx.annotation.RequiresApi
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.polito_mad_01.model.Invitation
 import com.example.polito_mad_01.model.Slot
 import com.example.polito_mad_01.repositories.InvitationRepository
 import com.example.polito_mad_01.repositories.ReservationRepository
+import com.example.polito_mad_01.repositories.UserRepository
 
-class InvitationsViewModel(private val invitationRepository: InvitationRepository,
-                           private val reservationRepository: ReservationRepository)
+class InvitationsViewModel( private val invitationRepository: InvitationRepository,
+                            private val reservationRepository: ReservationRepository)
     : ViewModel(){
 
         @RequiresApi(Build.VERSION_CODES.O)
         fun getReservations() : LiveData<List<Slot>>{
             return reservationRepository.getReservationsByUserId()
+        }
+
+        fun getUserInvitations() : LiveData<List<Invitation>> {
+            return invitationRepository.getUserInvitations()
         }
 }
 
