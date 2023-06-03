c8ff4630e186d16d50697b1c13c402b0952d804c
Edoardo Colella <edoardocolella.it@gmail.com>
2023-06-03 17:57:37 +0200

    fix achievements

--- app/src/main/java/com/example/polito_mad_01/model/UserData.kt
+++ app/src/main/java/com/example/polito_mad_01/model/UserData.kt
@@ -37,7 +37,7 @@ class UserData(
             birthdate,
             gender,
             location,
-            listOf(),
+            "",
             skills,
             image_uri,
             email,
